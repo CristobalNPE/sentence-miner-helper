eff2c9e39e220fb2b85f21f39be2fce46b41c4b3
Cristobal <cristobal.npe@gmail.com>
2022-06-15 23:12:07 -0400

    Add test functions to the clippings manager

--- untitled/src/Clippings.java
+++ untitled/src/Clippings.java
@@ -1,13 +1,16 @@
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class Clippings {
   private List<String> clippings;
+  private HashMap<String, ArrayList<String>> orderedClippings;
 
   public Clippings() {
     this.clippings = new ArrayList<>();
+    this.orderedClippings = new HashMap<>();
   }
 
   public void storeClippingsFromFile(String fileRoute) {
@@ -18,10 +21,32 @@ public class Clippings {
     }
   }
 
-  public void printClippings() {
-    for (String clipping : this.clippings) {
-      System.out.println(clipping);
+  /*Podriamos crear una clase Clipping, que almacene todos los datos del clip,
+   * incluyendo autor, fecha, el mismo clip.
+   * */
+  public void printClippingsWithStep(int step) {
+    for (int i = 3; i < this.clippings.size(); i = i + step) {
+      if (!this.clippings.get(i).isEmpty()) {
+        System.out.println(this.clippings.get(i));
+      }
+
     }
   }
+
+  public void getTitlesOfClippings() {
+
+    /*Por algun motivo esta funcion encuentra un libro duplicado, puede ser debido al japones*/
+    for (int i = 0; i < this.clippings.size(); i = i + 5) {
+      this.orderedClippings.putIfAbsent(this.clippings.get(i).trim(), new ArrayList<>());
+    }
+  }
+
+  public void printTitles() {
+    System.out.println(this.orderedClippings.size() + " found: \n");
+    for (String title : this.orderedClippings.keySet()) {
+      System.out.println(title);
+    }
+  }
+
 
 }
--- untitled/src/TextUI.java
+++ untitled/src/TextUI.java
@@ -9,7 +9,10 @@ public class TextUI {
   public void start() {
     System.out.println("Reading clippings...");
     this.clippings.storeClippingsFromFile("untitled/My Clippings.txt");
-    System.out.println("Printing clippings: ");
-    this.clippings.printClippings();
+    //this.clippings.printLines(3);
+    //this.clippings.printClippingsWithStep(5);
+    System.out.println("Titles found:");
+    this.clippings.getTitlesOfClippings();
+    this.clippings.printTitles();
   }
 }
